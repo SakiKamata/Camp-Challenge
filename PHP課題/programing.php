30e2c39c7ee9b7f0180e3ece15e1cb5163100d88
SakiKamata <tap.ucu.pi@gmali.com>
2015-12-15 17:31:59 +0900

    １２．１５課題作成分

---
+++ PHP課題/programing.php
@@ -0,0 +1,38 @@
+﻿<?php
+//課題1
+ $num=3;
+ $message='';
+ switch($num){
+  	case 1:
+   	 $message='one';
+	 echo $message;
+  	 break;
+	case 2:
+	 $message='two';
+	 echo $message;
+	 break;
+	default:
+	 $message='想定外<br>';
+	 echo $message;
+	 break;
+ }
+
+//課題2
+ $num=1;
+ $message='';
+ switch($num){
+	case 1:
+	 $message='英語<br>';
+	 echo $message;
+	 break;
+	case 2:
+	 $message='日本語';
+	 echo $message;
+	 break;
+}
+
+//課題3
+ for($i=8;$i<160;$i=$i*1){
+
+ }
+	
---
+++ PHP課題/test.php
@@ -0,0 +1,43 @@
+﻿<?php
+//課題１・２
+ echo 'Hello world.<br>';
+
+ echo 'groove'.'-'.'gear<br>';
+
+//課題3
+ echo '自己紹介<br>';
+ echo '名前：鎌田早紀<br>';
+ echo '家族：4人+1匹、2人姉妹の妹<br>';
+ echo '好きなこと：ドライブ<br>';
+
+//課題5.6
+ $a=5;
+ $b=10;
+ $c=2;
+ const A=1;
+ const B=15;
+ 
+ echo 10-$a;
+ echo 15+$b;
+ echo $c*A;
+ echo B/$a;
+ //結果
+ echo '10-$a=5<br>';
+ echo '15+$b=25<br>';
+ echo '$c*A=2<br>';
+ echo 'B/$a=3<br>';
+
+ $a=1;
+ if($a==1){
+ echo '1です!';
+
+}elseif($a==2){
+ echo 'プログラムキャンプ！';
+
+}elseif($a=='a'){
+ echo '文字です！';
+
+}else{
+ echo'その他です!';
+}
+?>
