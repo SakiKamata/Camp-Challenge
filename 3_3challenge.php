69c70e5bc22e715cbc229727f023b5f8aee51408
SakiKamata <tap.ucu.pi@gmali.com>
2015-12-17 13:26:43 +0900

    3_3challenge

---
+++ 3_3challenge.php
@@ -0,0 +1,17 @@
+﻿<?php
+//課題3
+ function hiki($a=1,$b=5,$type=false){
+
+ $ans=$a*$b;
+ $ruijyou=$ans*$ans;
+
+ if($type==false){
+	echo $ans;
+
+ }else{
+	echo $ruijyou;
+ }
+}
+ $y=5;$kake=25;
+ hiki($y,false);
+
